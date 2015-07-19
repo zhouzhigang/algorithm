67d9bcb3fd3f4ac950964c5fdacba628b02ba97b
Zhigang Zhou <zhouzhigang90s@gmail.com>
2015-07-19 09:00:52 -0400

    Init 12. Integer to Roman in LeetCode - Java.
    Init Java solution.
    Update related README.

---
+++ leetcode/integer-to-roman/IntegerToRoman.java
@@ -0,0 +1,72 @@
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        char[] basic = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int n = 4;
+        int k = 1000;
+        int h = 0;
+        while (n != 0) {
+            h = num / k;
+            switch(h) {
+                case 1:
+                    sb.append(basic[2*n-2]); break;
+                case 2:
+                    sb.append(basic[2*n-2]).append(basic[2*n-2]); break;
+                case 3:
+                    sb.append(basic[2*n-2]).append(basic[2*n-2]).append(basic[2*n-2]); break;
+                case 4:
+                    sb.append(basic[2*n-2]).append(basic[2*n-1]); break;
+                case 5:
+                    sb.append(basic[2*n-1]); break;
+                case 6:
+                    sb.append(basic[2*n-1]).append(basic[2*n-2]); break;
+                case 7:
+                    sb.append(basic[2*n-1]).append(basic[2*n-2]).append(basic[2*n-2]); break;
+                case 8:
+                    sb.append(basic[2*n-1]).append(basic[2*n-2]).append(basic[2*n-2]).append(basic[2*n-2]); break;
+                case 9:
+                    sb.append(basic[2*n-2]).append(basic[2*n]); break;
+                case 10:
+                    sb.append(basic[2*n]); break;
+                default:
+                    break;
+            }
+            num = num - h * k;
+            k /= 10;
+            n--;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman sol = new IntegerToRoman();
+
+        int[] nums = {
+            0, 1, 3, 5, 9, // 0 ~ 10
+            10, 12, 26, 43, 50, 98, // 10 ~ 100
+            100, 247, 707, 890, // 100 ~ 1000
+            1000, 2350, 3458, 3999 // 1000 ~ 3999
+        };
+
+        String[] results = {
+            "", "I", "III", "V", "IX", // 0 ~ 10
+            "X", "XII", "XXVI", "XLIII", "L", "XCVIII", // 10 ~ 100
+            "C", "CCXLVII", "DCCVII", "DCCCXC", // 100 ~ 1000
+            "M", "MMCCCL", "MMMCDLVIII", "MMMCMXCIX"
+        };
+
+        int count = nums.length;
+        int failed = 0;
+        String result;
+        for (int i = 0; i < count; i++) {
+            result = sol.intToRoman(nums[i]);
+            if (!result.equals(results[i])) {
+                failed++;
+                System.out.println("Test: " + nums[i] + ", expect: " + results[i] + ", while returned: " + result);
+            }
+        }
+        System.out.println("Test " + count + " cases: " + (count-failed) + " success, " + failed + " failed.");
+    }
+}
+
+
---
+++ leetcode/integer-to-roman/README.md
@@ -0,0 +1,28 @@
+# [Integer to Roman](https://leetcode.com/problems/integer-to-roman/)
+
+Tags:`Math`, `String`
+
+Difficulty: `Medium`
+
+## Description
+
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+## Analysis
+
+
+    1   2   3   4   5   6   7   8    9
+    I   II  III IV  V   VI  VII VIII IX
+
+    10  20  30  40  50  60  70  80   90
+    X   XX  XXX XL  L   LX  LXX LXXX XC 
+
+    100 500 700 900 1000
+    C   D   DCC CM  M
+
+There are seven basic symbols:
+    1   5   10  50  100 500 1000
+    I   V   X   L   C   D   M
+

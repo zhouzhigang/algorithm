0b5797f0a7d5c38959e893721764a7198d280d84
Zhigang Zhou <zhouzhigang90s@gmail.com>
2015-07-22 06:58:27 -0400

    Update 15. 3Sum in LeetCode - Java.
    Update Java solution.
    Update realated README.

--- leetcode/3sum/ThreeSum.java
+++ leetcode/3sum/ThreeSum.java
@@ -7,25 +7,36 @@ public class ThreeSum {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
         int right = nums.length -1;
-        int i, j, sum;
-        for (int k = 1; k < right; k++) {
-            i = 0; j = right;
-            while (i < k && k < j) {
-                // a+b+c = 0  <=> a+c = -b
-                sum = nums[i] + nums[j];
-                if (sum > -nums[k]) {
-                    j--;
-                } else if (sum < -nums[k]) {
-                    i++;
+        int i, j, k, sum;
+        for (i = 0; i < right - 1; i++) {
+            if (nums[i] > 0) break;
+
+            j = i+1; k = right;
+
+            while (j < k && nums[k] >= 0) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
                 } else {
                     List<Integer> triplet = new ArrayList<Integer>(3);
-                    // System.out.println(nums[i] + ", " + nums[k] + ", " + nums[j]);
+                    // System.out.println(nums[i] + ", " + nums[j] + ", " + nums[k]);
                     triplet.add(nums[i]);
-                    triplet.add(nums[k]);
                     triplet.add(nums[j]);
+                    triplet.add(nums[k]);
                     result.add(triplet);
-                    break;
+                    if (nums[j] != nums[k]) {
+                        j++;
+                        k--;
+                    } else {
+                        break; // skip same number
+                    }
                 }
+            }
+
+            while (i < right && nums[i] == nums[i+1]) {
+                i++; // skip same number
             }
         }
         return result;
@@ -35,10 +46,22 @@ public class ThreeSum {
         ThreeSum sol = new ThreeSum();
 
         int[][] nums = {
+            {1}, {-1, 1}, {-1, 1, 2}, // no such triple
+            {-1, -1, 2},
             {-1, 0, 1, 2, -1, 4},
+            {-1, 0, 1, 2, -1, -4},
+            {0, 0, 0, 0, 0}, // duplicate number
+            {-2, -1, 0, 1, 2},
+            {-2, -1, 0, 1, 1, 2},
         };
         int[] results = {
-            2
+            0, 0, 0, // no such triple
+            1,
+            2,
+            2,
+            1, // duplicate number
+            2,
+            3,
         };
         int count = results.length;
         int failed = 0;

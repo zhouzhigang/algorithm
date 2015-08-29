6519b29beed40558c7c50a79f00555332abccde7
Zhigang Zhou <zhouzhigang90s@gmail.com>
2015-08-29 21:53:34 +0800

    Update Insetion Sort

--- sorting/java/InsertionSort.java
+++ sorting/java/InsertionSort.java
@@ -1,20 +1,37 @@
-import java.util.Comparator;
+import java.util.Arrays;
 
 /**
  * Insertion Sort.
  */
 public class InsertionSort {
-    public static void insertionSort(Comparable[] a) {
-        int j;
-        for (int i = 1; i < a.length; i++) {
-            Comparable curr = a[i];
-            
-            for (j = i; j > 0; j--) {
-                if (curr.compareTo(a[j - 1]) < 0) {
-                    a[j] = a[j - 1];
-                }
+    public static void sort(int[] a) {
+        int i, j, curr;
+        for (i = 1; i < a.length; i++) {
+            curr = a[i];
+            for (j = i; j >0 && curr < a[j-1]; j--) {
+                a[j] = a[j - 1];
             }
             a[j] = curr;
         }
+    }
+
+    public static void sort(Comparable[] a) {
+        int i, j;
+        for (i = 1; i < a.length; i++) {
+            if (a[i].compareTo(a[i-1]) < 0) {
+                Comparable curr = a[i];
+                for (j = i; j > 0 && curr.compareTo(a[j - 1]) < 0; j--) {
+                    a[j] = a[j - 1];
+                }
+                a[j] = curr;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {2, 1, 5, 3, 6, 4, 8, 9, 7};
+        System.out.println(Arrays.toString(arr));
+        InsertionSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
